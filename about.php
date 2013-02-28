3d80dd352a027b1104431008db7d4cfced90d7ec
tbennett <tbennett@aii.edu>
2013-02-28 14:24:10 -0500

    added dynamic widget display to about page

--- about.php
+++ about.php
@@ -63,10 +63,20 @@ mysqli_close($db);
     </section>
     <section>
       <h2>Our Special Sauce</h2>
-      <p>An image placeholder was used in this layout in the header where you'll likely want to place  a logo. It is recommended that you remove the placeholder and replace it with your own linked logo. </p>
-      <p> Be aware that if you use the Property inspector to navigate to your logo image using the SRC field (instead of removing and replacing the placeholder), you should remove the inline background and display properties. These inline styles are only used to make the logo placeholder show up in browsers for demonstration purposes. </p>
-
-      <p>To remove the inline styles, make sure your CSS Styles panel is set to Current. Select the image, and in the Properties pane of the CSS Styles panel, right click and delete the display and background properties. (Of course, you can always go directly into the code and delete the inline styles from the image or placeholder there.)</p>
+      
+      
+      <?php
+	  	
+		//loop through $items and build div for each
+		for($i = 0; i < count($images); $i++)
+		{
+			echo '<div class="widget">';
+			echo $items[$i];
+			echo '</div>';
+		}
+	  
+	  ?>
+      
     </section>
   <!-- end .content --></article>
 
